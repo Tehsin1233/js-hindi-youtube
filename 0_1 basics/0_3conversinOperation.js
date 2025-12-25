aad1f61ff31a3f6c557ee2cd40f8db95309aca2c
Tehsin1233 <kotharitehsin12345@gmail.com>
2025-12-25 13:57:19 +0000

    conversion of datatypes

--- 0_1 basics/0_3conversinOperation.js
+++ 0_1 basics/0_3conversinOperation.js
@@ -24,8 +24,41 @@ let BooleanisLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber); 
-console.log(typeof stringNumber); 
+// console.log(stringNumber); 
+// console.log(typeof stringNumber); 
+
+// ************** OPERATIONS   *******************
+
+let value = 3
+
+let Negvalue = -value
+// console.log(Negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")    
+
+
+// console.log(true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
 
 
