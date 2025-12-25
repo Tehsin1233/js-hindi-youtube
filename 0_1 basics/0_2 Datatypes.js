b26a462502db8f8eb27aadfdc03c232c9e5addc0
Tehsin1233 <kotharitehsin12345@gmail.com>
2025-12-25 10:00:54 +0000

    datatypes

---
+++ 0_1 basics/0_2 Datatypes.js
@@ -0,0 +1,30 @@
+// "Use Strict"; treat all JS Code as newer version
+
+// alert(3 + 3) we are using node.js, not browser
+
+console.log(3 
+    +
+     3) //code readability should be high
+
+
+     let name = "Tehsin"
+     let age = 19
+     let isLoggedIn = true
+     let state;
+
+    //  number => 2 to power 53
+    // bigint
+    // String => ""
+    // Boolean => true/false
+    // null => StandAlone value
+    // Undefined => 
+    // Symbol =>unique
+
+    // Object 
+
+    console.log(typeof"tehsin")
+    console.log(typeof age)
+    // console.log(typeof null) Object
+    // console.log(typeof undefined) undefined
+
+    
