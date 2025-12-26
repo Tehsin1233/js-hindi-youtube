6deffe7a20a990ac2b4e2ab542b4dfd92f45b337
Tehsin1233 <kotharitehsin12345@gmail.com>
2025-12-26 16:10:23 +0000

    String

---
+++ 0_1 basics/0_5 String.js
@@ -0,0 +1,37 @@
+const name = "tehsin"
+const RepoCount = 50
+
+// console.log(name + RepoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${RepoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(1,4)
+// console.log(newString);
+
+const anotherstring = gameName.slice(-8,4)
+// console.log(anotherstring);
+
+const newStringOne = "  hitesh  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('hitesh'));
+// console.log(url.includes('tehsin'));
+
+console.log(gameName.split('-'));
