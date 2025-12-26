a800a5191fecd059f49a86a8b34103e967a1f726
Tehsin1233 <kotharitehsin12345@gmail.com>
2025-12-26 12:04:10 +0000

    Datatype Summary

---
+++ 0_1 basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, udefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetempt = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id == anotherid)
+
+// const bigNumber = 3458795121365n
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+ let myobj = {
+    name: "hitesh",
+    age : 22,
+}
+
+const myfunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof anotherid);
