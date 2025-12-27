2fe467fedb1d16ef3a1f273b469670a6909ef89b
Tehsin1233 <kotharitehsin12345@gmail.com>
2025-12-27 03:40:20 +0000

    ConversionandOperation

--- 0_1 basics/0_3conversinOperation.js
+++ 0_1 basics/0_3conversinOperation.js
@@ -31,8 +31,8 @@ let stringNumber = String(someNumber)
 
 let value = 3
 
-let Negvalue = -value
-// console.log(Negvalue);
+let negValue = -value
+// console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -53,11 +53,12 @@ let str3 = str1 + str2
 // console.log(1 + 2 + "2")    
 
 
-// console.log(true);
-// console.log(+"");
+console.log(true);
+console.log(+"");
 
 let gameCounter = 100
-++gameCounter;
+// gameCounter++;
+// ++gameCounter;
 console.log(gameCounter);
 
 
