7bfde41d57317e62dc3c2f4d767bf4f06b54c46b
Tehsin1233 <kotharitehsin12345@gmail.com>
2025-12-27 10:29:19 +0000

    MathandNums

--- 0_1 basics/0_6 nums_and_Math.js
+++ 0_1 basics/0_6 nums_and_Math.js
@@ -27,10 +27,10 @@ console.log(Math);
 
 
 console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
 
-Math.random() * (max-min + 1)
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
