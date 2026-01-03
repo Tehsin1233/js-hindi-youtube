ac7703b9f917058c8fd46cffe503a5bda965d690
Tehsin1233 <kotharitehsin12345@gmail.com>
2026-01-03 02:21:07 +0000

    IF ELSE

---
+++ 0_4_control_flow/one.js
@@ -0,0 +1,54 @@
+// IF
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if (temperature === 41) {
+//     console.log("Less than 50");
+// }else{
+
+//     console.log("temperature is greather than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=,  ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power : ${power}`);
+    
+// }
+//     console.log(`User power : ${power}`);
+
+
+// const balance = 2000
+// // if(balance > 500) console.log("test"),console.log("test2")
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance < 750) {
+//     console.log("less than 500");
+    
+// }else if (balance < 900) {
+//     console.log("less than 900");
+// }else {
+//     console.log("less than 1200");
+// }
+
+
+const UserLoggedIn = true
+const debitcard = true
+const LoggedInFromGoogle = false 
+const LoggedInFromEmail = true
+
+if (LoggedInFromGoogle || LoggedInFromEmail) {
+    console.log("User logged in ");
+    
+}
+
+
+// if (UserLoggedIn && debitcard && 2==2){
+//     console.log("Allow to buy course");
+// }
