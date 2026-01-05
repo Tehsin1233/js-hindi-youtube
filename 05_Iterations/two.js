e122853ed1151daf64d77868aac2cc8e87580004
Tehsin1233 <kotharitehsin12345@gmail.com>
2026-01-05 06:33:02 +0000

    while loops

---
+++ 05_Iterations/two.js
@@ -0,0 +1,21 @@
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`the value of myarrays ${myArray[arr]}`);
+    arr += 1    
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 10);
