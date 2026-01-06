0a87538883acedccc7053bb3f9075bde26fdcbbc
Tehsin1233 <kotharitehsin12345@gmail.com>
2026-01-06 12:33:25 +0000

    Nums and Math in JS

--- 0_1 basics/0_6 nums_and_Math.js
+++ 0_1 basics/0_6 nums_and_Math.js
@@ -4,12 +4,14 @@ const score = 400
 
 const balance = new Number(100)
 // console.log(balance);
+// console.log(typeof balance);
+
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(3));
 
 const otherNumber = 123.8966
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
 
 
 const hundreds = 1000000
@@ -17,7 +19,7 @@ const hundreds = 1000000
 
 // ++++++++++++++ MATHS +++++++++++++++++++
 
-console.log(Math);
+// console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.5));
 // console.log(Math.ceil(4.2));
@@ -26,11 +28,11 @@ console.log(Math);
 // console.log(Math.max(4,2,8,6,));
 
 
-console.log(Math.random());
+// console.log(Math.random());
 // console.log((Math.random()*10) + 1);
 // console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
+// console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
